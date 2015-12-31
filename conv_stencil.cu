8c5b08cef36d8f368d88cd5d4a7fbc801849a1f0
LinZichuan <linzichuan12@163.com>
2015-12-31 17:57:18 +0800

    Add multi input-feature maps C, with output-feature maps K=1

--- conv_stencil.cu
+++ conv_stencil.cu
@@ -4,13 +4,14 @@
 #include <cublas_v2.h>
 using namespace std;
 #define real float
-#define N 2
-#define H 32
-#define W 32
+#define N 3
+#define C 2
+#define H 5
+#define W 5
 #define R 3
 #define S 3
-#define BX 16
-#define BY 16
+#define BX 1
+#define BY 1
 #define OH (H-R+1)
 #define OW (W-S+1)
 #define checkCUDNNError(status) \
@@ -39,14 +40,14 @@ void setup() {
     checkCUDNNError(cudnnCreate(&cudnnHandle));
     checkCUDNNError(cudnnSetTensor4dDescriptor(bottom_desc_,
                     CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT,
-                    N, 1, H, W));
+                    N, C, H, W));
     checkCUDNNError(cudnnSetTensor4dDescriptor(top_desc_,
                     CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT,
-                    N, 1, H-R+1, W-S+1));
+                    N, 1, OH, OW)); //如果没有多个神经元，此处的output feature maps 应该等于1 ！！！否则cudnn会输出多个channel而导致结果错误！！
     checkCUDNNError(cudnnSetFilter4dDescriptor(filter_desc_,
-                    CUDNN_DATA_FLOAT, 1, 1, R, S));
+                    CUDNN_DATA_FLOAT, 1, C, R, S)); //k,c,h,w  k=output_feature_maps; c=input_feature_maps
     checkCUDNNError(cudnnSetConvolution2dDescriptor(conv_desc_,
-                    0, 0, 1, 1, 1, 1, CUDNN_CONVOLUTION));
+                    0, 0, 1, 1, 1, 1, CUDNN_CONVOLUTION)); //pad_h, pad_w, u(vertical stride), v(horizonal stride), upscalex=1, upscaley=1
 
     checkCUDNNError(cudnnGetConvolutionForwardAlgorithm(cudnnHandle,
                     bottom_desc_, filter_desc_, conv_desc_, top_desc_,
@@ -65,9 +66,11 @@ __global__ void baseline(real* input, real* output, real* K, int outh, int outw)
     if (i < W-S+1 && j < H-R+1) { 
         for (int n = 0; n < N; ++n) {
             tmp = 0;
-            for (int a = 0; a < R; ++a) {
-                for (int b = 0; b < S; ++b) {
-                    tmp += input[((j+a)*W+(i+b)) + n*H*W] * K[(R-1-a)*S + (S-1-b)];
+            for (int c = 0; c < C; ++c) {
+                for (int a = 0; a < R; ++a) {
+                    for (int b = 0; b < S; ++b) {
+                        tmp += input[((j+a)*W+(i+b)) + n*H*W*C + c*H*W] * K[(R-1-a)*S + (S-1-b) + c*R*S];
+                    }
                 }
             }
             output[j*outw+i + n*OH*OW] = tmp;
@@ -126,74 +129,78 @@ void cpu_comp(real* input, real* output, real* K, int h, int w, int kr, int ks, 
         for (int i = 0; i < outh; ++i) {
             for (int j = 0; j < outw; ++j) {
                 real a = 0;
-                for (int r = 0; r < kr; ++r) {
-                    for (int s = 0; s < ks; ++s) {
-                        //a += input[(i+r)*w + (j+s)] * K[(kr-1-r)*ks+(ks-1-s)];
-                        a += input[((i+r)*w+(j+s)) + n*H*W] * K[(kr-1-r)*ks+(ks-1-s)];
+                for (int c = 0; c < C; ++c) {
+                    for (int r = 0; r < kr; ++r) {
+                        for (int s = 0; s < ks; ++s) {
+                            //a += input[(i+r)*w + (j+s)] * K[(kr-1-r)*ks+(ks-1-s)];
+                            a += input[((i+r)*w+(j+s)) + n*H*W*C + c*H*W] * K[(kr-1-r)*ks+(ks-1-s) + c*R*S];
+                        }
                     }
                 }
+                //output[(i*outw+j) + n*OH*OW*C + c*OH*OW] = a;
                 output[(i*outw+j) + n*OH*OW] = a;
             }
         }
     }
 }
 bool check(real* A, real* B, int size) {
-    int Nsize = N * size;
+    int Nsize = N*size;
     for (int i = 0; i < Nsize; ++i)
         if (A[i] != B[i]) {
-            printf("ERROR at %d: %d %d\n", i, A[i], B[i]);
+            printf("ERROR at %d: %f %f\n", i, A[i], B[i]);
             return false;
         }
     return true;
 }
 int main() {
     setup();
-    int insize = N*H*W*sizeof(float);
-    int outsize = N*(H-R+1)*(W-S+1)*sizeof(float); // total out size
-    int ksize = S*R*sizeof(float);
+    int insize = N*C*H*W*sizeof(float);
+    int outsize = N*OH*OW*sizeof(float); // total out size
+    int ksize = C*S*R*sizeof(float);
     int outh = H - R + 1;  //each out size
     int outw = W - S + 1;
-    real* host_input = (real*)malloc(insize);
+    real* host_input = (real*)malloc(insize); //malloc是按字节分配！大小要乘以字节数！
     real* host_baseline_output = (real*)malloc(outsize);
     real* host_output = (real*)malloc(outsize);
     real* cpu_output = (real*)malloc(outsize);
     real* host_k = (real*)malloc(ksize);
+    //init input matrix && output matrix
     for (int n = 0; n < N; ++n) {
         //n=0,1
-        for (int i = 0; i < H; ++i) {
-            for (int j = 0; j < W; ++j) {
-                host_input[i*W+j+H*W*n] = j;
+        for (int c = 0; c < C; ++c) {
+            for (int i = 0; i < H; ++i) {
+                for (int j = 0; j < W; ++j) {
+                    host_input[i*W+j + n*C*H*W + c*H*W] = j;
+                }
             }
         }
         for (int i = 0; i < outh; ++i) {
             for (int j = 0; j < outw; ++j) {
-                host_output[i*outw+j+outh*outw*n] = 0;
-                host_baseline_output[i*outw+j+outh*outw*n] = 0;
+                host_output[i*outw+j + n*outh*outw] = 0;
+                host_baseline_output[i*outw+j + n*outh*outw] = 0;
             }
         }
     }
-    //k
-    for (int i = 0; i < R; ++i) {
-        for (int j = 0; j < S; ++j) {
-            host_k[i*S+j] = i*S+j+1;
+    //init kernel(neuron)
+    //k=1, c=0,1
+    for (int c = 0; c < C; ++c) {
+        for (int i = 0; i < R; ++i) {
+            for (int j = 0; j < S; ++j) {
+                host_k[i*S+j + c*R*S] = i*S+j+1;
+            }
         }
     }
     cpu_comp(host_input, cpu_output, host_k, H, W, R, S, outh, outw);
-    /*printf("cpu result:\n");*/
-    /*for (int i = 0; i < outh; ++i) {*/
-        /*for (int j = 0; j < outw; ++j) {*/
-            /*printf("%f ", cpu_output[i*outw+j]);*/
-        /*}*/
-    /*printf("\n");*/
-    /*}*/
     printf("start...\n");
     printf("---------------------\n");
+    //GPU malloc
     real *dev_input, *dev_output, *dev_k;
     cudaMalloc(&dev_input, insize);
     cudaMalloc(&dev_output, outsize);
     cudaMalloc(&dev_k, ksize);
     cudaMemcpy(dev_input, host_input, insize, cudaMemcpyHostToDevice);
     cudaMemcpy(dev_k, host_k, ksize, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_output, host_output, outsize, cudaMemcpyHostToDevice);
     dim3 threadPerBlock(BX, BY);
     dim3 blockPerGrid((outw+BX-1)/BX, (outh+BY-1)/BY);
     //init
@@ -214,6 +221,7 @@ int main() {
     else 
         printf("baseline error!\n");
     printf("---------------------\n");
+
     //opt_register
     /*cudaEventRecord(start, 0);*/
     /*opt_register<<<blockPerGrid, threadPerBlock>>>(dev_input, dev_output, dev_k, outh, outw);*/
@@ -224,6 +232,7 @@ int main() {
     /*cudaMemcpy(host_output, dev_output, outsize, cudaMemcpyDeviceToHost);*/
     /*check(cpu_output, host_output, outh*outw);*/
     /*printf("---------------------\n");*/
+
     //cudnn
     cudaEventRecord(start, 0);
     checkCUDNNError(cudnnConvolutionForward(cudnnHandle, &alpha, bottom_desc_,
@@ -235,12 +244,34 @@ int main() {
     cudaEventElapsedTime(&time, start, stop);
     printf("cudnn: time = %fms \n", time);
     cudaMemcpy(host_output, dev_output, outsize, cudaMemcpyDeviceToHost);
-    cpu_comp(host_input, cpu_output, host_k, H, W, R, S, outh, outw);
+
     if (check(cpu_output, host_output, outh*outw)) 
         printf("cudnn correct!\n");
     else
         printf("cudnn error!\n");
-    printf("---------------------\n");
+
+    /*printf("cudnn result:\n");*/
+    /*for (int n = 0; n < N; ++n) {*/
+        /*for (int i = 0; i < OH; ++i) {*/
+            /*for (int j = 0; j < OW; ++j) {*/
+                /*printf("%f ", host_output[i*OW+j + n*OH*OW]);*/
+            /*}*/
+            /*printf("\n");*/
+        /*}*/
+        /*printf(">>>\n");*/
+    /*}*/
+    /*printf("---------------------\n");*/
+    /*printf("cpu result:\n");*/
+    /*for (int n = 0; n < N; ++n) {*/
+        /*for (int i = 0; i < outh; ++i) {*/
+            /*for (int j = 0; j < outw; ++j) {*/
+                /*printf("%f ", cpu_output[i*outw+j + n*outh*outw]);*/
+            /*}*/
+            /*printf("\n");*/
+        /*}*/
+        /*printf(">>>\n");*/
+    /*}*/
+    /*printf("\n");*/
 
     return 0;
 }
